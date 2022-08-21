0d6c68e09fe30b9212222fb8e25c49130c617ad3
AltugAlkan <alkanaltug92@gmail.com>
2022-08-21 15:28:25 +0300

    devamke

--- Odev_20_08_v2.1/Odev_20_08_v2.1/Form1.cs
+++ Odev_20_08_v2.1/Odev_20_08_v2.1/Form1.cs
@@ -15,6 +15,12 @@ namespace Odev_20_08_v2._1
         public Form1()
         {
             InitializeComponent();
+            this.label1.ForeColor= System.Drawing.Color.HotPink;
+
+            Label lbl_isim=new Label();
+            lbl_isim.AutoSize = true;     // true yaparsan cümleyi ne kadar uzatırsan o kadar büyür :D
+            lbl_isim.Text = "İsim bla bla bla bla";
+            this.Controls.Add(lbl_isim);
         }
     }
 }
